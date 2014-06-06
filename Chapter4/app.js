5b6153219e2468425e66a2a5d8aa54188d1ec3fc
Robert Bender <robertbender@gmail.com>
2014-06-06 16:45:39 -0400

    3rd commit

---
+++ Chapter4/app.js
@@ -0,0 +1,20 @@
+var main = function() {
+	"use strict";
+
+	$(".comment-input button").on("click", function (event) {
+		var $new_comment; // = $("<p>").text($(".comment-input input").val());
+
+		//$new_comment.text(comment_text);
+		if ($(".comment-input input").val() !== "") {
+			$new_comment = $("<p>").text($(".comment-input input").val());
+			$(".comments").append($new_comment);		
+		}
+
+
+		console.log("Hello World!");
+	});
+
+
+};
+
+$(document).ready(main);
