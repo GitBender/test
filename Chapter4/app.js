945d1aedf4bdf799c864cf941a083b658d31ab5c
Robert Bender <robertbender@gmail.com>
2014-06-10 12:52:04 -0400

    3rd

--- Chapter4/app.js
+++ Chapter4/app.js
@@ -1,19 +1,27 @@
 var main = function() {
 	"use strict";
 
-	$(".comment-input button").on("click", function (event) {
-		var $new_comment; // = $("<p>").text($(".comment-input input").val());
+	var addCommentFromInputBox = function () {
+		var $new_comment;
 
-		//$new_comment.text(comment_text);
 		if ($(".comment-input input").val() !== "") {
 			$new_comment = $("<p>").text($(".comment-input input").val());
-			$(".comments").append($new_comment);		
+			$new_comment.hide();
+			$(".comments").append($new_comment);
+			$new_comment.fadeIn();
+			$(".comment-input input").val(""); 		//get rid of txt in box
 		}
+	};
 
-
-		console.log("Hello World!");
+	$(".comment-input button").on("click", function (event) {
+		addCommentFromInputBox();
 	});
 
+	$(".comment-input input").on("keypress", function (event) {
+		if (event.keyCode === 13) {
+			addCommentFromInputBox();
+		}
+	});
 
 };
 
